5ea3e5ec35ff04878493c8b2b8998d193cc3accd
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2018-11-12 00:38:01 +0900

    [modify] 画像パスを修正

--- createjs/drop-item/js/game.js
+++ createjs/drop-item/js/game.js
@@ -16,7 +16,7 @@ var Player = (function() {
     container.addChild(shape);
 
     var ss = new createjs.SpriteSheet({
-      images: ['/createjs/drop-item/images/player.png'],
+      images: ['/game/createjs/drop-item/images/player.png'],
       frames: [
         [0, 0, 500, 500, 0, 0, 0], // x, y, width, height, imageIndex*, regX*, regY*
         [0, 500, 500, 500, 0, 0, 0],
